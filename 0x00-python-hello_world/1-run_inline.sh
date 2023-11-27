eff017d3a2b32825579ab155790cf62679755910
Muluken <mulukeneshetu70@gmail.com>
2023-11-27 06:20:38 -0800

    Python - Hello, World

---
+++ 0x00-python-hello_world/1-run_inline.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set the environment variable PYCODE
+export PYCODE='print(f"Best School: {88+10}")'
+
+# Run the Python code saved in the environment variable PYCODE
+python -c "$PYCODE"
+
