e566e0517f175d9d80eb7d0bdf0f04b68dffb70b
Muluken <mulukeneshetu70@gmail.com>
2023-11-30 19:56:52 -0800

    Python - import & modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 import dis
 import importlib.util
 
+
 def print_names():
     spec = importlib.util.spec_from_file_location("hidden_4", "./hidden_4.pyc")
     module = importlib.util.module_from_spec(spec)
@@ -11,6 +12,7 @@ def print_names():
     for name in sorted(names):
         if not name.startswith("__"):
             print(name)
+
 
 if __name__ == "__main__":
     print_names()
