839f254a3d115e0959b80cbd25753959e49151d5
Muluken <mulukeneshetu70@gmail.com>
2023-12-03 10:31:08 -0800

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,69 +1,31 @@
 #include "lists.h"
-#include <stddef.h>
 
 /**
-* reverse_list - Reverses a linked list.
-* @head: Pointer to the head of the linked list.
-*/
-void reverse_list(listint_t **head);
-
-/**
-* compare_lists - Compares two linked lists.
-* @list1: Pointer to the head of the first linked list.
-* @list2: Pointer to the head of the second linked list.
-*
-* Return: 1 if the lists are the same, 0 otherwise.
-*/
-int compare_lists(listint_t *list1, listint_t *list2);
-
-/**
-* is_palindrome - Checks if a singly linked list is a palindrome.
-* @head: Pointer to the head of the linked list.
-*
-* Return: 1 if it is a palindrome, 0 otherwise.
-*/
+ * is_palindrome - function to call check_pal to see if list is palindrome
+ * @head: ptr to the beginning of the list
+ * Return: 0 if not palindrome else 1
+ */
 int is_palindrome(listint_t **head)
 {
-listint_t *slow_ptr = *head, *fast_ptr = *head;
-listint_t *prev_slow = *head;
-listint_t *second_half, *mid_node = NULL;
-int is_palindrome = 1;
-
-if (*head != NULL && (*head)->next != NULL)
-{
-/* Use two pointers to find the middle of the linked list */
-while (fast_ptr != NULL && fast_ptr->next != NULL)
-{
-fast_ptr = fast_ptr->next->next;
-prev_slow = slow_ptr;
-slow_ptr = slow_ptr->next;
+	if (head == NULL || *head == NULL)
+		return (1);
+	return (check_pal(head, *head));
 }
 
-/* If the number of elements is odd, move to the next element */
-if (fast_ptr != NULL)
+/**
+ * check_pal - function to check if the list is palindrome
+ * @head: ptr to the beginning of the list
+ * @last: ptr to the end of the list
+ * Return: 0 if not palindrom else 1
+ */
+int check_pal(listint_t **head, listint_t *last)
 {
-mid_node = slow_ptr;
-slow_ptr = slow_ptr->next;
-}
-
-/* Reverse the second half of the linked list */
-reverse_list(&slow_ptr);
-
-/* Compare the first half and the reversed second half */
-is_palindrome = compare_lists(*head, slow_ptr);
-
-/* Reconstruct the original linked list */
-reverse_list(&slow_ptr);
-if (mid_node != NULL)
-{
-prev_slow->next = mid_node;
-mid_node->next = slow_ptr;
-}
-else
-{
-prev_slow->next = slow_ptr;
-}
-}
-
-return (0);
+	if (last == NULL)
+		return (1);
+	if (check_pal(head, last->next) && (*head)->n == last->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
 }
