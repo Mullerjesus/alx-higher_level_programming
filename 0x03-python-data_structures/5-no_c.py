d3da211f07634c0b9b0902831c09bfff27d62ee9
Muluken <mulukeneshetu70@gmail.com>
2023-12-03 07:29:01 -0800

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
-    return new_string
+    new = ""
+    for i in my_string:
+        if i not in ['c', 'C']:
+            new += i
+    return new
