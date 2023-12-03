86d21d0b5ff40ffb50533ea5de88cc9a28dbef35
Muluken <mulukeneshetu70@gmail.com>
2023-12-03 07:45:59 -0800

    Python - Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
+
+# Example usage:
+if __name__ == "__main__":
+    sentence = "At school, I learnt C!"
+    length, first = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(length, first))
