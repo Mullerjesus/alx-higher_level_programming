f0b5d56caea6ad5a5015ee682f82c3704d88f0ca
Muluken <mulukeneshetu70@gmail.com>
2023-12-19 12:13:27 -0800

    Python - Classes and Objects

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
+"""
+Square Class Definition.
+"""
 
 
 class Square:
     """
-    This is the Square class.
+    Defines the properties of a square.
 
     Attributes:
-        __size (int): The size of the square.
+        __size (int): Represents the size of a square (1 side).
     """
 
     def __init__(self, size=0):
         """
-        Initializes a new instance of the Square class.
+        Initializes new square instances.
 
-        Parameters:
-            size (int): The size of the square.
+        Args:
+            size (int): Represents the size of the square (1 side).
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("Size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("Size must be a non-negative integer")
         else:
             self.__size = size
