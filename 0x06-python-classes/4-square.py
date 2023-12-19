e9684688152f550239974cca204cbf6ca07607b8
Muluken <mulukeneshetu70@gmail.com>
2023-12-19 12:35:28 -0800

    Python - Classes and Objects

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,36 +16,12 @@ class Square:
         Args:
             size (int): The size of the square.
         """
-        self.size = size
+        self._Square__size = size
 
-    @property
-    def size(self):
-        """
-        Getter method to retrieve the size of the square.
-
-        Returns:
-            int: The size of the square.
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """
-        Setter method to set the size of the square.
-
-        Args:
-            value (int): The size of the square.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("Size must be an integer")
-        elif value < 0:
-            raise ValueError("Size must be >= 0")
-        else:
-            self.__size = value
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
 
     def area(self):
         """
@@ -54,18 +30,18 @@ class Square:
         Returns:
             int: The area of the square.
         """
-        return self.__size ** 2
+        return self._Square__size ** 2
 
 
 if __name__ == "__main__":
     my_square = Square(89)
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+    print("Area: {} for size: {}".format(my_square.area(), my_square._Square__size))
 
-    my_square.size = 3
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+    my_square._Square__size = 3
+    print("Area: {} for size: {}".format(my_square.area(), my_square._Square__size))
 
     try:
-        my_square.size = "5 feet"
-        print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+        my_square._Square__size = "5 feet"
+        print("Area: {} for size: {}".format(my_square.area(), my_square._Square__size))
     except Exception as e:
         print(e)
