a7289e7fdeece8e116e2ba349f219d053313073a
Muluken <mulukeneshetu70@gmail.com>
2023-12-19 12:25:48 -0800

    Python - Classes and Objects

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Square Class Definition.
-"""
+"""Square Class Definition."""
+
 
 
 class Square:
@@ -40,21 +39,3 @@ class Square:
             int: The area of the square.
         """
         return self.__size ** 2
-
-
-if __name__ == "__main__":
-    my_square_1 = Square(3)
-    print("Area: {}".format(my_square_1.area()))
-
-    try:
-        print(my_square_1.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
-
-    my_square_2 = Square(5)
-    print("Area: {}".format(my_square_2.area()))
