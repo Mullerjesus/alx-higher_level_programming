569ac4382758db8ced2072df217199377ff02127
Muluken <mulukeneshetu70@gmail.com>
2023-12-19 10:17:03 -0800

    Python - Classes and Objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    This is the Square class.
+
+    Attributes:
+        No attributes at the moment.
+    """
+
     pass
 
 if __name__ == "__main__":
