e582af2c36a4d5756b505024f1d01170e6ef5d31
Muluken <mulukeneshetu70@gmail.com>
2023-12-19 12:23:01 -0800

    Python - Classes and Objects

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,29 +1,60 @@
 #!/usr/bin/python3
+"""
+Square Class Definition.
+"""
+
 
 class Square:
     """
-    Defines a square with a private instance attribute and a public instance method.
+    Represents a square with a private instance attribute and a public instance method.
     """
 
     def __init__(self, size=0):
         """
         Initializes a new instance of the Square class.
 
-        Parameters:
+        Args:
+            size (int): The size of the square.
+        """
+        self.__set_size(size)
+
+    def __set_size(self, size):
+        """
+        Private method to set the size of the square.
+
+        Args:
             size (int): The size of the square.
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("Size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("Size must be >= 0")
         else:
             self.__size = size
 
     def area(self):
         """
-        Computes the area of the square.
+        Calculates the area of the square.
 
         Returns:
             int: The area of the square.
         """
         return self.__size ** 2
+
+
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    print("Area: {}".format(my_square_1.area()))
+
+    try:
+        print(my_square_1.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square_1.__size)
+    except Exception as e:
+        print(e)
+
+    my_square_2 = Square(5)
+    print("Area: {}".format(my_square_2.area()))
