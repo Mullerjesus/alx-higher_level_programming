2681df5b508879aae7b69db46d06dcb36148dd22
Muluken <mulukeneshetu70@gmail.com>
2024-01-04 14:15:15 -0800

    Python - More Classes and Objects

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python3
+"""Defines a Rectangle class."""
+
 class Rectangle:
-def __init__(self, width=0, height=0):
-self.width = width
-self.height = height
+    """Represents a rectangle shape."""
 
-@property
-def width(self):
-return self.__width
+    def __init__(self, width=0, height=0):
+        """Initializes a rectangle with a given width and height."""
+        self.width = width
+        self.height = height
 
-@width.setter
-def width(self, value):
-if not isinstance(value, int):
-raise TypeError("width must be an integer")
-elif value < 0:
-raise ValueError("width must be >= 0")
-self.__width = value
+    @property
+    def width(self):
+        """Retrieves the width of the rectangle."""
+        return self.__width
 
-@property
-def height(self):
-return self.__height
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-@height.setter
-def height(self, value):
-if not isinstance(value, int):
-raise TypeError("height must be an integer")
-elif value < 0:
-raise ValueError("height must be >= 0")
-self.__height = value
+    @property
+    def height(self):
+        """Retrieves the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
