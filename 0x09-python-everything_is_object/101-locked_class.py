36329688199386db0901760bc31e673b73b9a4bc
Muluken <mulukeneshetu70@gmail.com>
2024-01-06 04:31:36 -0800

    Python - Everything is object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-class LockedClass:
-    __slots__ = ['first_name']
+def __setattr__(self, key, value):
+    if not hasattr(self, key) and key != 'first_name':
+        raise AttributeError("Cannot create new instance attribute")
+    super().__setattr__(key, value)
