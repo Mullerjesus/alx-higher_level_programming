66f806c13dc1609b331d2307167917393c46a0de
Muluken <mulukeneshetu70@gmail.com>
2024-01-09 17:54:27 -0800

    Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-
-
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read())
