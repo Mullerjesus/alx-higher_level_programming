a933d544046ba26638f3629adb9759524413ee5a
Muluken <mulukeneshetu70@gmail.com>
2024-01-09 18:13:04 -0800

    Python - Input/Output

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 """
-Contains the "from_json_string" function
+Contains the "to_json_string" fundtion
 """
 
 import json
 
-def from_json_string(json_string):
-    """
-    Returns an object represented by a JSON string.
 
-    Args:
-        json_string: the JSON string to be deserialized
-
-    Returns:
-        The deserialized object from the input JSON string
-    """
-    return json.loads(json_string)
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
