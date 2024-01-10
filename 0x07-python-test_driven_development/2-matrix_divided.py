87159494a71ef1caf4edb15b94c747ba55a9944e
Muluken <mulukeneshetu70@gmail.com>
2024-01-10 13:33:16 -0800

    Python - Test-driven development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
+"""matrix divider module"""
 def matrix_divided(matrix, div):
-    errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
-    if not matrix:
-        raise TypeError(errorMessage)
-    if not isinstance(matrix, list):
-        raise TypeError(errorMessage)
-    for lists in matrix:
-        if not isinstance(lists, list):
-            raise TypeError(errorMessage)
-        for item in lists:
-            if not isinstance(item, int) and not isinstance(item, float):
-                raise TypeError(errorMessage)
-    for lists in matrix:
-        if len(lists) == 0:
-            raise TypeError(errorMessage)
-    if not isinstance(div, int) and not isinstance(div, float):
-        raise TypeError("div must be a number")
-    if not all(len(lists) == len(matrix[0]) for lists in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    return [[round(item / div, 2) for item in lists] for lists in matrix]
+    """divides a matrix all elements by divider
+    """
+    if(not isinstance(matrix,list) or not len(matrix) or
+       0 in [len(listx) if type(listx) is list else 0 for listx in matrix] or
+       any(False in x for x in  [[isinstance(ele,(int,float)) for ele in row]
+       for row in matrix])):
+        raise TypeError(
+            'matrix must be a matrix (list of lists) of integers/floats')
+    if(len(set([len(listx) for listx in matrix])) > 1):
+        raise TypeError(
+            'Each row of the matrix must have the same size')
+    if(not isinstance(div,(int,float))):
+        raise TypeError('div must be a number')
+    if(div is 0):
+        raise ZeroDivisionError('division by zero')
+    return [[round(ele / div, 2) for ele in row] for row in matrix]
