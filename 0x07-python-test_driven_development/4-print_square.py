7dc32c9feab01a9fdbca678fae02936beb2251bb
Muluken <mulukeneshetu70@gmail.com>
2024-01-10 14:02:19 -0800

    Python - Test-driven development

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-'''print square module'''
+"""
+This is the "4-print_square" module.
+The 4-print_square  module supplies one function, print_square(size).
+"""
+
+
 def print_square(size):
-    if not isinstance(size,int):
-        raise TypeError('size must be an integer')
+    """prints a square with "#"'s that has a length of size """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError('size must be >= 0')
-    print('\n'.join('#' * size for x in range(size)))
-print_square(4)
-print("")
-print_square(10)
-print("")
-print_square(0)
-print("")
-print_square(1)
-print("")
-try:
-    print_square(-1)
-except Exception as e:
-    print(e)
-print("")
+        raise ValueError("size must be >= 0")
+    if size > 0:
+        print(("#" * size + "\n") * size, end="")
