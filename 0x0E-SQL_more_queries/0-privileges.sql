195f247a1e38e33815ddd5748b9ca6ba7db6fbc4
Muluken <mulukeneshetu70@gmail.com>
2024-02-16 03:19:28 -0800

    SQL - More queries

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
+-- lists all privileged for user 1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- lists all privileged for user
+SHOW GRANTS FOR user_0d_2@localhost;
