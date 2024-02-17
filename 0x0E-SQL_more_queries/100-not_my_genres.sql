32ea5471c174d82447ab5ab6f5036bf14c3ed648
Muluken <mulukeneshetu70@gmail.com>
2024-02-17 09:16:10 -0800

    SQL - More queries

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,5 +2,5 @@
 SELECT tv_genres.name 
 FROM tv_genres 
 WHERE id NOT IN 
-(SELECT genre_id FROM tv_shows JOIN tv_genres WHERE title = "Dexter");
+SELECT genre_id FROM tv_shows JOIN tv_genres WHERE title = "Dexter";
 ORDER BY tv_genres.name ASC;
