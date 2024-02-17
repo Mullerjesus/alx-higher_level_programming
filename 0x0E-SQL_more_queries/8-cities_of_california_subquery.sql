399a2d430c465c6556779bd8a82fabb470c157db
Muluken <mulukeneshetu70@gmail.com>
2024-02-17 08:21:13 -0800

    SQL - More queries

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,2 @@
--- script creates db hbtn_0d_usa and the table cities
--- create db 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; 
--- use db
-USE hbtn_0d_usa 
--- create table in db
-CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL, 
-state_id INT NOT NULL, 
-name VARCHAR(255) NOT NULL, 
-PRIMARY KEY(id), 
-FOREIGN KEY(state_id) REFERENCES states(id));
+-- lists all cities of California found in db hbtn_0d_usa
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
